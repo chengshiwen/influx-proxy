62abd425c1dd97e60797956b3fec8926ba8cf78f
shi0730 <54302869+shi0730@users.noreply.github.com>
2021-05-28 17:39:08 +0800

    Update proxy.go
    添加对保存策略操作的支持

--- backend/proxy.go
+++ backend/proxy.go
@@ -105,6 +105,9 @@ func (ip *Proxy) Query(w http.ResponseWriter, req *http.Request) (body []byte, e
 		return QueryDeleteOrDropQL(w, req, ip, tokens, db)
 	} else if alterDb {
 		return QueryAlterQL(w, req, ip)
+	} else if CheckRetentionPolicy(tokens) {
+		// 保存策略操作 by lyj
+		return QueryRetentionPolicyQL(w, req, ip)
 	}
 	return nil, ErrIllegalQL
 }
